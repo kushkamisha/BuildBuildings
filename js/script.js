9d2ed48cd0d1dea7301f3ea238c68c5041385d29
Misha <kushkamisha@yahoo.com>
2017-09-24 01:44:36 +0000

    Morning commit

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ $('#two').click(function() {
 $('#three').click(function() {
     $('#diagram').attr('src','img/3.png');
 })
+
+// Fill div's with numbers.
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 , 12, 13]
+
+// var arr1 = ['4%', '21°C', '24°C', '900', '58', '60%', '150', '7', '25%', '30%', '80%', '50%']
+// var arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 , 12, 13]
+// var arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 , 12, 13]
+
+for (var i = 0; i < arr.length; i++) {
+    $('#numbers div:nth-child(' + i + ')').text(arr[i])
+    // $('#numbers div:nth-child(' + i + ')').css('left', i*20 + 'px')
+    // $('#numbers div:nth-child(' + i + ')').css('top', i*20 + 'px')
+}
